eaa46839fe529c2190a91cc6073f5c6f952a2ea5
Rahul Saini - Web Designer & Developer <52367202+irahulsaini@users.noreply.github.com>
2024-06-26 21:49:37 +0530

    update

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -140,6 +140,28 @@ $('.stepCarousel,.projectCarousel').owlCarousel({
         }
     }
 });
+
+$('.portfolioCarousel').owlCarousel({
+    loop:false,
+    margin:10,
+    dots:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1,
+            dots:true,
+        },
+        600:{
+            items:3,
+            dots:true,
+        },
+        1000:{
+            items:3,
+            dots:true,
+        }
+    }
+});
+
 /*
 $(window).scroll(function(){
   var scroll = $(window).scrollTop();
